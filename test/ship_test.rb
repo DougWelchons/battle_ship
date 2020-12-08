7412534b751709306e5aa4203fdab81f7eba2631
DougWelchons <doug.welchons@gmail.com>
2020-12-07 18:52:21 -0700

    Add test 2 for ship test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -10,9 +10,13 @@ class ShipTest < Minitest::Test
     assert_instance_of Ship, cruiser
   end
 
-  # def test_it
-  # assert_equal "Cruiser", cruiser.name
-  # assert_equal 3, cruiser.length
+  def test_it_has_name
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal "Cruiser", cruiser.name
+  end
+
+  #assert_equal 3, cruiser.length
   #
   #
   # pry(main)> cruiser.health
