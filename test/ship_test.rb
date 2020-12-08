ecc9f16ab8345b0e76f90dbced1650ec402b5e7e
DougWelchons <doug.welchons@gmail.com>
2020-12-07 18:37:02 -0700

    Add Ship test file

---
+++ test/ship_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+  end
+
+  # def test_it
+  # assert_equal "Cruiser", cruiser.name
+  # assert_equal 3, cruiser.length
+  #
+  #
+  # pry(main)> cruiser.health
+  # #=> 3
+  #
+  # pry(main)> cruiser.sunk?
+  # #=> false
+  #
+  # pry(main)> cruiser.hit
+  #
+  # pry(main)> cruiser.health
+  # #=> 2
+  #
+  # pry(main)> cruiser.hit
+  #
+  # pry(main)> cruiser.health
+  # #=> 1
+  #
+  # pry(main)> cruiser.sunk?
+  # #=> false
+  #
+  # pry(main)> cruiser.hit
+  #
+  # pry(main)> cruiser.sunk?
+  # #=> true
+
+
+end
