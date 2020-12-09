4f3098ed250b3b76e766257de8fee500eb9abc9e
Katy La Tour <klatour324@gmail.com>
2020-12-08 21:41:45 -0600

    Create fired upon and fire upon methods for Cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -5,6 +5,15 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
+    @print_to_screen = {[true, true, false] => "M",
+                        [false, true, false] => "H",
+                        [true, false, false] => ".",
+                        [false, false, false] => ".",
+                        [true, true, true] => "M",
+                        [false, true, true] => "H",
+                        [true, false, true] => ".",
+                        [false, false, true] => "S"}
   end
 
   def empty?
@@ -14,4 +23,48 @@ class Cell
   def place_ship(ship)
     @ship = ship
   end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+   @ship.hit if !empty?
+   @fired_upon = true
+  end
 end
+
+
+
+
+
+
+
+
+
+#   def render(cheat = false)
+#     sunk? ? "X" : @prent_to_screen[[empty?, fired_upon?, cheat]]
+#   end
+#
+# def render(cheat = false)
+#   if sunk?
+#     "X"
+#   else
+#   @prent_to_screen[[empty?, fired_on?, cheat]]
+# end
+#
+#
+# if sunk?
+#   "x"
+# elsif empty? && fired_upon?
+#   "m"
+# elsif if empty?
+#   "."
+# elsif fired_upon?
+#   "h"
+# elsif cheat == true
+#   "s"
+# else
+#   "."
+# end
+#
