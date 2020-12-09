e1d7e1d5745586d76194e9f1448dbda4e7c0b5e6
Katy La Tour <klatour324@gmail.com>
2020-12-08 21:39:32 -0600

    Create tests for firing upon cell

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -34,4 +34,26 @@ class CellTest < Minitest::Test
     assert_equal cruiser, cell.ship
     assert_equal false, cell.empty?
   end
+
+  def test_it_has_not_been_fired_upon_by_default
+   cruiser = Ship.new("Cruiser", 3)
+   cell = Cell.new("B4")
+
+   cell.place_ship(cruiser)
+
+   assert_equal false, cell.fired_upon?
+  end
+
+  def test_it_has_been_fired_upon
+   cruiser = Ship.new("Cruiser", 3)
+   cell = Cell.new("B4")
+
+   cell.place_ship(cruiser)
+
+   assert_equal false, cell.fired_upon?
+
+   cell.fire_upon
+
+   assert_equal true, cell.fired_upon?
+  end
 end
