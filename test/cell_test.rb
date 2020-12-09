3b09bbf2b60f3c3510a94a0f769e60fe4435c735
Katy La Tour <klatour324@gmail.com>
2020-12-08 22:46:48 -0600

    Create tests to render cells

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -47,7 +47,7 @@ class CellTest < Minitest::Test
   def test_it_has_been_fired_upon
    cruiser = Ship.new("Cruiser", 3)
    cell = Cell.new("B4")
-
+   # require "pry"; binding.pry
    cell.place_ship(cruiser)
 
    assert_equal false, cell.fired_upon?
@@ -56,4 +56,52 @@ class CellTest < Minitest::Test
 
    assert_equal true, cell.fired_upon?
   end
+
+  def test_it_renders_if_it_has_not_been_fired_upon
+    cell_1 = Cell.new("B4")
+    cell_2 = Cell.new("C3")
+    # require "pry"; binding.pry
+
+    assert_equal ".", cell_1.render
+    assert_equal ".", cell_2.render
+  end
+
+  def test_it_renders_if_it_has_been_fired_upon_but_does_not_contain_a_ship
+    cell_1 = Cell.new("B4")
+
+    cell_1.fire_upon
+
+    assert_equal "M", cell_1.render
+  end
+
+  # def test_it_renders_if_it_has_been_fired_upon_and_contains_a_ship
+    # cell_2 = Cell.new("C3")
+
+  # end
+
+
+
+  # Indicate that we want to show a ship with the optional argument
+  # cell_1 = Cell.new("B4")#
+  # cell_2 = Cell.new("C3")
+  #
+  # assert_equal "S", cell_2.render(true)
+
+  # pry(main)> cell_2.fire_upon
+
+  # pry(main)> cell_2.render
+  # => "H"
+
+  # pry(main)> cruiser.sunk?
+  # => false
+
+  # pry(main)> cruiser.hit
+
+  # pry(main)> cruiser.hit
+
+  # pry(main)> cruiser.sunk?
+  # => true
+
+  # pry(main)> cell_2.render
+  # => "X"
 end
