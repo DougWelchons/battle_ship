fbb03183572642790dd7f331d3a462a0c7983717
DougWelchons <doug.welchons@gmail.com>
2020-12-08 20:25:20 -0700

    Add test 2 and 3 for board test file

--- test/board_test.rb
+++ test/board_test.rb
@@ -15,4 +15,17 @@ class BoardTest < Minitest::Test
 
     assert_equal 16, board.cells.count
   end
+
+  def test_it_has_valid_coordinate
+    board = Board.new
+
+    assert_equal true, board.valid_coordinate?("A1")
+  end
+
+  def test_it_has_invalid_coordinate
+    board = Board.new
+
+    assert_equal false, board.valid_coordinate?("C5")
+  end
+
 end
