61bb677cdcf6ae4a82b34d9805aa9f33978e21a9
DougWelchons <doug.welchons@gmail.com>
2020-12-08 19:06:00 -0700

    Add test 2 for board test file

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,4 +9,10 @@ class BoardTest < Minitest::Test
 
     assert_instance_of Board, board
   end
+
+  def test_it_has_16_cells
+    board = Board.new
+
+    assert_equal 16, board.cells.count
+  end
 end
