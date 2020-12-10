f134c6c57bef052cc9c87df49b775e920117be99
Katy La Tour <klatour324@gmail.com>
2020-12-09 23:13:52 -0600

    Create tests that validate ship placements

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/board'
+require './lib/ship'
 
 class BoardTest < Minitest::Test
 
@@ -28,40 +29,77 @@ class BoardTest < Minitest::Test
     assert_equal false, board.valid_coordinate?("C5")
   end
 
-  def test_it_has_valid_placements
+  def test_it_is_invalid_if_not_the_length_of_the_ship
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false, board.valid_length?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_length?(submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_it_is_valid_if_it_is_the_length_of_the_ship
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal true, board.valid_length?(cruiser, ["A1", "A2", "A3"])
+    assert_equal true, board.valid_length?(submarine, ["B2", "C2"])
+  end
+
+  def test_it_is_invalid_if_on_a_diagonal
+    board = Board.new
+
+    assert_equal false, board.consecutive?(["A1", "B2", "C3"])
+    assert_equal false, board.consecutive?(["C2", "D3"])
+  end
+
+  def test_it_is_valid_if_not_on_a_diagonal
+    board = Board.new
+
+    assert_equal true, board.consecutive?(["A1", "A2" "A3"])
+    assert_equal true, board.consecutive?(["C2", "D2"])
+  end
+
+  def test_it_has_invalid_placements_in_non_consecutive_order
+    board = Board.new
+
+    assert_equal false, board.consecutive?(["A1", "A2", "A4"])
+    assert_equal false, board.consecutive?(["C1", "B1"])
+  end
+
+  def test_it_has_valid_placements_in_consecutive_order
+    board = Board.new
+
+    assert_equal true, board.consecutive?(["A1", "A2", "A3"])
+    assert_equal true, board.consecutive?(["B1", "C1"])
+  end
+
+  def test_it_validates_placement
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
 
     assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
-    assert_equal true, board.valid_placement?(submarine, ["B2", "C2"])
+    assert_equal false, board.valid_placement?(submarine, ["B2", "D2"])
   end
-
-  def test_it_has_invalid_placements #overlaping
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
-    assert_equal true, board.valid_placement?(submarine, ["A2", "B2"])
-  end
-
-  def test_it_has_invalid_placements #partially off of the board
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    assert_equal true, board.valid_placement?(cruiser, ["A3", "A4", "A5"])
-    assert_equal true, board.valid_placement?(submarine, ["B2", "C2"])
-  end
-
-  def test_it_has_invalid_placements #nonsequential cells
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    assert_equal true, board.valid_placement?(submarine, ["B2", "C2"])
-  end
-
 end
+#
+# def test_it_has_invalid_placements #overlaping
+#   board = Board.new
+#   cruiser = Ship.new("Cruiser", 3)
+#   submarine = Ship.new("Submarine", 2)
+#
+#   assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
+#   assert_equal false, board.valid_placement?(submarine, ["A2", "B2"])
+# end
+#
+# def test_it_has_invalid_placements #partially off of the board
+#   board = Board.new
+#   cruiser = Ship.new("Cruiser", 3)
+#   submarine = Ship.new("Submarine", 2)
+#
+#   assert_equal false, board.valid_placement?(cruiser, ["A3", "A4", "A5"])
+#   assert_equal true, board.valid_placement?(submarine, ["B2", "C2"])
+# end
+#
