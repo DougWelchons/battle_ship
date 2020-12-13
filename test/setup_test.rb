0e5944eb79eb9b12cea4fb2dba78c1d61263ea4b
Katy La Tour <klatour324@gmail.com>
2020-12-12 21:16:25 -0600

    Create tests to collect ships and give ships to players

---
+++ test/setup_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cell'
+require './lib/setup'
+
+class SetupTest < Minitest::Test
+
+  def test_it_exists
+    setup = Setup.new
+
+    assert_instance_of Setup, setup
+  end
+
+  def test_it_can_collect_ships
+    setup = Setup.new
+
+    setup.collect_ships
+
+    assert_equal 2, setup.ships.count
+  end
+
+  def test_it_can_give_ships_to_the_computer_and_player
+    setup = Setup.new
+
+    setup.give_ships
+
+    assert_equal 2, setup.player.ships.count
+    assert_equal 2, setup.computer.ships.count
+    refute_equal setup.computer.ships, setup.player.ships
+  end
+end
