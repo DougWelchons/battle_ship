7b7f4c7afe1c53d8066fc5139fad6b7c40a05b74
Katy La Tour <klatour324@gmail.com>
2020-12-14 19:45:41 -0600

    Create mock and stubs for player shoots test

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/ship'
 require './lib/board'
 require './lib/computer'
@@ -17,5 +18,20 @@ class TurnTest < Minitest::Test
     turn = Turn.new(computer, computer_board, player, player_board)
 
     assert_instance_of Turn, turn
+  end
+
+  def test_for_player_shoots
+    computer = mock
+    player = mock
+    computer_board = mock
+    player_board = mock
+    turn = Turn.new(computer, computer_board, player, player_board)
+
+    player.stubs(:single_coordinate_input).returns("A1")
+    computer_board.stubs(:valid_target?).returns(true)
+    computer_board.expects(:fire_upon)
+    player.expects(:stored_input)
+
+    turn.player_shoots
   end
 end
