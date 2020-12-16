7fa622d9ad7c5b6e452d0b7458a0550ba2761536
Katy La Tour <klatour324@gmail.com>
2020-12-16 15:09:18 -0600

    Refactor Computer class methods

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,16 +1,13 @@
 class Computer
   attr_reader :computer_board,
+              :ships,
               :stored_input,
-              :ships
+              :valid_targets
 
   def initialize(computer_board)
     @computer_board = computer_board
     @ships = []
     @valid_targets = computer_board.cells.keys.shuffle
-  end
-
-  def add_ships(ships)
-    @ships = ships
   end
 
   def has_lost?
@@ -19,33 +16,35 @@ class Computer
     end
   end
 
+  def add_ships(ships)
+    @ships = ships
+  end
+
   def place_ship(ship, ship_length)
-    while @computer_board.place(ship, generator(ship_length)) == false
+    while @computer_board.place(ship, coordinates_generator(ship_length)) == false
     end
   end
 
-  def generator(ship_length)
+  def coordinates_generator(ship_length)
     placement = ["horizontal", "vertical"]
     if placement.sample == "vertical"
-      vertical_generator(ship_length)
+      vertical_coordinates_generator(ship_length)
     else
-      horizontal_generator(ship_length)
+      horizontal_coordinates_generator(ship_length)
     end
   end
 
-  def vertical_generator(ship_length)
-    starting_coordinate = @computer_board.cells.keys.sample
-    coordinates = [starting_coordinate]
+  def vertical_coordinates_generator(ship_length)
+    coordinates = [@computer_board.cells.keys.sample]
     (ship_length - 1).times do
       coordinates << "#{coordinates.last[0].next}#{coordinates.last[1]}"
     end
     coordinates
   end
 
-  def horizontal_generator(ship_length)
-    starting_coordinate = @computer_board.cells.keys.sample
-    coordinates = [starting_coordinate]
-    (ship_length-1).times do
+  def horizontal_coordinates_generator(ship_length)
+    coordinates = [@computer_board.cells.keys.sample]
+    (ship_length - 1).times do
       coordinates << "#{coordinates.last[0]}#{coordinates.last[1].to_i + 1}"
     end
     coordinates
