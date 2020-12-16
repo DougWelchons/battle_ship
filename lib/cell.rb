95d10838b2a36d0d3ac0978aa42b000298209091
DougWelchons <doug.welchons@gmail.com>
2020-12-15 22:30:45 -0700

    Add Cell class refactors

--- lib/cell.rb
+++ lib/cell.rb
@@ -7,47 +7,42 @@ class Cell
     @ship = nil
     @fired_upon = false
     @print_to_screen = {
-      [true, true, false] => "M",
-      [false, true, false] => "H",
-      [true, false, false] => ".",
-      [false, false, false] => ".",
-      [true, true, true] => "M",
-      [false, true, true] => "H",
-      [true, false, true] => ".",
-      [false, false, true] => "S"
-}
+                        [true,  false, false, false] => ".",
+                        [false, false, false, false] => ".",
+                        [true,  true,  false, false] => "M",
+                        [false, true,  false, false] => "H",
+                        [false, true,  true,  false] => "X",
+                        [true,  false, false, true] => ".",
+                        [false, false, false, true] => "S",
+                        [true,  true,  false, true] => "M",
+                        [false, true,  false, true] => "H",
+                        [false, true,  true,  true] => "X"
+                        }
   end
 
   def empty?
     @ship.nil?
   end
 
-  def place_ship(ship)
-    @ship = ship
-  end
-
   def fired_upon?
     @fired_upon
+  end
+
+  def sunk?
+    return false if @ship.nil?
+    @ship.sunk?
+  end
+
+  def render(reveal = false)
+    @print_to_screen[[empty?, fired_upon?, sunk?, reveal]]
+  end
+
+  def place_ship(ship)
+    @ship = ship
   end
 
   def fire_upon
    @ship.hit if !empty?
    @fired_upon = true
-  end
-
-  def sunk?
-    if @ship.nil?
-      false
-    else
-      @ship.sunk?
-    end
-  end
-
-  def render(cheat = false)
-    if sunk? == true
-      "X"
-    else
-      @print_to_screen[[empty?, fired_upon?, cheat]]
-    end
   end
 end
